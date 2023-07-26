084fdcaecdfe9d6b00c269089d6f01f1eaff3f6a
ahg80 <ah.golrokhi@gmail.com>
2023-07-26 13:23:59 +0330

    fixing signUpForm pass equality check

--- src/pages/SignUpForm.jsx
+++ src/pages/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import "./SignUpForm.css";
+/*import "./SignUpForm.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 //import { Link } from "react-router-dom";
@@ -32,8 +32,8 @@ const SignUpForm = () => {
     setPassIsEqual(password1 === password2);
     if(!passIsEqual){
       try{
-        //setFormData({
-        //  ...formData,
+        //setformdata({
+        //  ...formdata,
         //  [event.target.name]: event.target.value
         //});
         console.log(formData);
@@ -97,4 +97,97 @@ const SignUpForm = () => {
   );
 };
 
+export default SignUpForm;*/
+
+import "./SignUpForm.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const SignUpForm = () => {
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+
+  const handleInputChange = (event, setInput) => {
+    const { name, value } = event.target;
+    setInput(value);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    if (password1 !== password2) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    try {
+      const formData = {
+        email: email,
+        password: password1
+      };
+
+      const response = await axios.post("http://localhost:4000/register", formData);
+      const token = response.data.accessToken;
+      console.log(token);
+      localStorage.setItem('token', token);
+      alert('SignUp successful! Moving you to the login page.');
+      navigate("/login");
+    } catch (error) {
+      console.log(error.response.data);
+      alert(error.response.data);
+    }
+
+    setPassword1('');
+    setPassword2('');
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="mx-auto col-10 col-md-8 col-lg-6">
+          <div className="form-box">
+            <form className="form" onSubmit={submitHandler}>
+              <span className="title">Sign up</span>
+              <hr />
+              <span className="subtitle">
+                Create a free account with your email.
+              </span>
+              <div className="form-container">
+                <input type="email" name="email" className="input" placeholder="Email" value={email} onChange={(event) => handleInputChange(event, setEmail)} />
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Password"
+                  value={password1}
+                  onChange={(event) => handleInputChange(event, setPassword1)}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  className="input"
+                  placeholder="Repeat password"
+                  value={password2}
+                  onChange={(event) => handleInputChange(event, setPassword2)}
+                />
+              </div>
+              <button type="submit">Sign up</button>
+            </form>
+            <div className="form-section">
+              <p>
+                Have an account? <Link to={"login"}>Log in</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default SignUpForm;
+
